abcad2cec2936feedf019c2eaf5b7c4766111712
isawurth <51935955+isawurth@users.noreply.github.com>
2021-04-21 21:10:09 -0300

    Add files via upload

---
+++ aula3_ADID6/src/test/java/br/edu/ifsp/aula3_ADID6/Aula3Adid6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aula3_ADID6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula3Adid6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
